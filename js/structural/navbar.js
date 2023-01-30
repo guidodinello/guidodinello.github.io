c6ef5554e8f050c8f9dda3e8d39a23c64e91d321
Guido Dinello <guido.dinello@gmail.com>
2023-01-30 03:36:32 -0300

    contact list style event

--- js/structural/navbar.js
+++ js/structural/navbar.js
@@ -2,8 +2,11 @@ const CV_PATH = "../../resources/Guido Dinello.pdf"
 
 const brandHTML = `<a class="navbar-brand mx-auto" href="index.html">Guido Dinello</a>`;
 const dropdownName = "More";
-const dropdownItem = (name, url = "#", onclick = "") => {
-    return `<li><a class="dropdown-item" href="${url}" onclick="return ${onclick}();">${name}</a></li>`;
+const dropdownItem = (name, url = "", onclick = "") => {
+    return `<li><a class="dropdown-item" 
+        ${url==""? "" : `href="${url}"`} 
+        ${onclick==""? "" : `onclick="${onclick}();"`}
+        >${name}</a></li>`;
 }
 const downloadableDropdownItem = (name, url) => {
     return `<li><a class="dropdown-item" href="${url}" download>${name}</a></li>`;
@@ -21,21 +24,28 @@ const searchBar = `
     </form>
 </div>`
 
-/*
-would be nicer to iterate over the list of social media logos and make a "wave" effect
-along with some coloring
-*/
-const contactItemAction = () => {
-    const contactList = document.getElementById("contactList");
-    contactList.scrollIntoView({behaviour:"smooth"})
-    contactList.style.transition = "all .2s ease-in-out"; 
-    contactList.style.transform = "scale(2.0)";
-    const oldColor = contactList.style.color;
-    contactList.style.background = "yellow";
-    setTimeout(() => {
-        contactList.style.transform = "scale(1.0)";
-        contactList.style.background = oldColor;
-    }, 1500);
+const contactItemWaveAction = () => {
+    const contactList = document.querySelector("#contactList");
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
+    
+    const interval = 350; // how much time should the delay between two iterations be
+    contactList.querySelectorAll("li").forEach((li, index) => {
+        // delay between each iteration
+        setTimeout( () => {
+            li.style.transition = "all .45s ease-in-out";
+            li.style.transform = "scale(2.0)";
+
+            const icon = li.querySelector("i");
+            const oldColor = icon.style.color;
+            icon.style.color = "orange";
+
+            // restore old values
+            setTimeout(() => {
+                li.style.transform = "scale(1.0)";
+                icon.style.color = oldColor;
+            }, interval*1.5);
+        }, index * interval);
+    });
 }
 
 const togglerBtn =`
@@ -68,7 +78,7 @@ const header = `
                     ${dropdownName}
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end text-center">
-                    ${dropdownItem("Contact", "#","contactItemAction")}
+                    ${dropdownItem("Contact", "","contactItemWaveAction")}
                     ${downloadableDropdownItem("my_cv.pdf", CV_PATH, "")}
                 </ul>
             </li>
