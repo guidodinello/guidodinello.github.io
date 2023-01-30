96b48e52e59946c74e524973c6eb7488095d5b68
Guido Dinello <guido.dinello@gmail.com>
2023-01-30 02:55:22 -0300

    footer improved, email added

--- js/structural/footer.js
+++ js/structural/footer.js
@@ -3,22 +3,26 @@ const twitter = "";
 const github = "https://github.com/guidodinello";
 const facebook = "";
 const linkedin = "https://www.linkedin.com/in/guido-dinello-48875b217/";
+const email = "guido.dinello@gmail.com";
 
 const footer = `
 <div class="container">
 <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top row bg-light">
 
     <div class="col-sm-4 order-2 order-sm-0 d-flex justify-content-center  align-items-center">
-        <small class="mb-3 mb-md-0 text-muted">© 2022 All rights reserved</small>
+        <small class="mb-3 mb-md-0 text-muted">© 2023 All rights reserved</small>
     </div>
 
-    <a href="/"
+    <a href="mailto:${email}?subject=Contact%20from%20personal%20website"
         class="col-sm-4 order-1 order-sm-1 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none fw-bold">
-        Guido
+        <div class="text-center">
+        <span>Guido</span><br>
+        <span class="text-muted fw-light" style="font-size:14px;"><u>${email}</u></span>
+        </div>
     </a>
 
     <ul
-        class="nav col-sm-4 order-0 order-sm-2 justify-content-center list-unstyled mb-3 mb-sm-0 d-flex grow-icons">
+        id="contactList" class="nav col-sm-4 order-0 order-sm-2 justify-content-center list-unstyled mb-3 mb-sm-0 d-flex grow-icons">
         <li class="ms-3"><a class="text-muted" target="_blank" href="${instagram}"><i class="fab fa fa-instagram fa-xl"></i></a></li>
         <li class="ms-3"><a class="text-muted" target="_blank" href="${twitter}"><i class="fab fa fa-twitter fa-xl"></i></a></li>
         <li class="ms-3"><a class="text-muted" target="_blank" href="${github}"><i class="fab fa fa-github fa-xl"></i></a></li>
