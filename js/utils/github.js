7f9caa0960952f7707bf5347bdfa5d3fcf1d8af2
Guido Dinello <guido.dinello@gmail.com>
2023-02-03 15:14:51 -0300

    go to deployed site button test

--- js/utils/github.js
+++ js/utils/github.js
@@ -9,35 +9,35 @@ export async function getRepos({element: spinnerWrapper, toggleClass: tcList}) {
     return await fetch('https://api.github.com/users/guidodinello/repos')
     .then(response => response.json())
     .then(data => { 
-        return data.map( repo => {
+        return data.map( async (repo) => {
+            const url = getGithubPagesUrl(repo["name"])
+            //.then(url => { return url })
+            console.log("URL ", url, "type ", typeof(url))
             return {
                 "title" : repo["name"],
                 "description" : repo["description"],
                 "url" : repo["html_url"],
                 "tags" : repo["topics"],
                 "language" : repo["language"],
-                "deploy" : repo["homepage"] || getGithubPagesUrl(repo["name"]),
+                "deploy" : repo["homepage"] || url,
             }
         })
     })
     .catch(error => {
-        console.log("ERROR while fetchhing user repos: ", error);
+        console.log("ERROR while fetching user repos: ", error);
     })
 }
 
 async function getGithubPagesUrl(repo_name) {
     /* workaround, github api doesnt return gh pages url with the repo info */
-    const response = await fetch(`https://guidodinello.github.io/${repo_name}`, { method: 'HEAD' });
-
-    // 404 page doesnt exist
-    if (response.status == '404') {
-        return;    
-    }
-
-    if (!response.ok) {
+    fetch(`https://cors-anywhere.herokuapp.com/https://guidodinello.github.io/${repo_name}`, { method: 'HEAD' })
+    .then(response => {
+        console.log("repo ", repo_name, "response ", response)
+        if (response.status == '404') { console.log("Repo not found"); return; }
+        console.log("return url ")
+        return `https://guidodinello.github.io/${repo_name}`
+    })
+    .catch(error => {
         console.log(error);
-        return;
-    }        
-
-    return `https://guidodinello.github.io/${repo_name}`
+    })
 }
