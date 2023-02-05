e612a8e6ca62e7a94120ee50455cb63ff311683d
Guido Dinello <guido.dinello@gmail.com>
2023-02-04 21:02:14 -0300

    go to deploy button added

--- js/utils/github.js
+++ js/utils/github.js
@@ -1,4 +1,4 @@
-export async function getRepos({element: spinnerWrapper, toggleClass: tcList}) {
+export function getRepos({element: spinnerWrapper, toggleClass: tcList}) {
     if (spinnerWrapper) {
         spinnerWrapper.classList.remove(tcList);
         setTimeout(() => {
@@ -6,22 +6,19 @@ export async function getRepos({element: spinnerWrapper, toggleClass: tcList}) {
         }, 400);        
     }
 
-    return await fetch('https://api.github.com/users/guidodinello/repos')
+    return fetch('https://api.github.com/users/guidodinello/repos')
     .then(response => response.json())
-    .then(data => { 
-        return data.map( async (repo) => {
-            const url = getGithubPagesUrl(repo["name"])
-            //.then(url => { return url })
-            console.log("URL ", url, "type ", typeof(url))
+    .then( data => { 
+        return Promise.all(data.map(async (repo) => {
             return {
                 "title" : repo["name"],
                 "description" : repo["description"],
                 "url" : repo["html_url"],
                 "tags" : repo["topics"],
                 "language" : repo["language"],
-                "deploy" : repo["homepage"] || url,
+                "deploy" : repo["homepage"] || await getGithubPagesUrl(repo["name"]),
             }
-        })
+        }));
     })
     .catch(error => {
         console.log("ERROR while fetching user repos: ", error);
@@ -30,14 +27,12 @@ export async function getRepos({element: spinnerWrapper, toggleClass: tcList}) {
 
 async function getGithubPagesUrl(repo_name) {
     /* workaround, github api doesnt return gh pages url with the repo info */
-    fetch(`https://guidodinello.github.io/${repo_name}`, { method: 'HEAD' })
-    .then(response => {
-        console.log("repo ", repo_name, "response ", response)
-        if (response.status == '404') { console.log("Repo not found"); return; }
-        console.log("return url ")
-        return `https://guidodinello.github.io/${repo_name}`
-    })
-    .catch(error => {
-        console.log(error);
-    })
+    try{
+        const response = await fetch(`https://guidodinello.github.io/${repo_name}`, { method: 'HEAD' })
+        if (response.status === 404) return null;
+        else return `https://guidodinello.github.io/${repo_name}`;
+    }
+    catch(error){
+        console.log("ERROR with REPO", repo_name);
+    }
 }
