5939e5748b1010a9ac82072b6836f56d2149c571
Guido Dinello <guido.dinello@gmail.com>
2023-02-13 23:00:59 -0300

    pageSpinner show-hide events changed

--- js/components/spinner.js
+++ js/components/spinner.js
@@ -1,10 +1,38 @@
-export function wrapper(parent = document.body) {
-    const wrapper = document.createElement('div');
-    wrapper.id = "spinnerWrapper";
-    wrapper.innerHTML = `
-    <div id="spinner" class="spinner-border text-success" role="status">
-        <span class="visually-hidden">Loading...</span>
-    </div>`;
-    parent.appendChild(wrapper);
-    return wrapper;
+export class SpinnerWrapper {
+    constructor(id, parentNode) {
+        this.id = this.#sanitize(id);
+        this.spinnerWrapper = this.#element();
+        if (parentNode) 
+            parentNode.appendChild(this.spinnerWrapper)
+    }
+
+    show() {
+        this.spinnerWrapper.classList.remove('d-none');
+    }
+    hide() {
+        this.spinnerWrapper.classList.add('d-none');
+    }
+
+    DOMreference() {
+        return this.spinnerWrapper;
+    }
+
+    id() {
+        return this.id;
+    }
+
+    #element() {
+        const wrapper = document.createElement('div');
+        wrapper.setAttribute('id', this.id);
+        wrapper.classList.add('d-none');
+        wrapper.innerHTML = `
+        <div id="spinner" class="spinner-border text-success" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>`;
+        return wrapper;
+    }
+
+    #sanitize(id) {
+        return id.replace(/[^a-zA-Z0-9]/g, '');
+    }
 }
