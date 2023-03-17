d5b1b8bc098c82f5671f6fe984f8d964724f69b4
guidodinello <88291553+guidodinello@users.noreply.github.com>
2023-03-17 19:58:01 -0300

    fixed typo

--- js/functional/index.js
+++ js/functional/index.js
@@ -2,7 +2,7 @@ import { titledCard } from "../components/titled-card.js";
 
 const mainContainer = document.querySelector("#mainContainer");
 mainContainer.appendChild(
-    titledCard("About Me", "I am a software developer with a passion for learning and creating new things. I am currently studying Computer Science at the University of the Oriental Republic of Uruguay.", "", { href: "projects.html", text: "See my projects" })
+    titledCard("About Me", "I am a software developer with a passion for learning and creating new things. Currently, I'm studying Computer Science at the University of the Eastern Republic of Uruguay.", "", { href: "projects.html", text: "See my projects" })
 );
 mainContainer.appendChild(
     titledCard("About this page", "This page is a collection of my projects. It is built using HTML, CSS, JavaScript and Bootstrap and hosted on GitHub Pages.", "")
