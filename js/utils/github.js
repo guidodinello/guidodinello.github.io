7a1a97c18fbf96b997d7f24c1aa33bf37310bc69
Guido Dinello <guido.dinello@gmail.com>
2023-06-23 01:50:39 -0300

    coding style modifications

--- js/utils/github.js
+++ js/utils/github.js
@@ -1,34 +1,35 @@
-export function getRepos() {
-    return fetch("https://api.github.com/users/guidodinello/repos")
-        .then((response) => response.json())
-        .then((data) => {
-            return Promise.all(
-                data.map(async (repo) => {
-                    return {
-                        title: repo["name"],
-                        description: repo["description"],
-                        url: repo["html_url"],
-                        tags: repo["topics"],
-                        language: repo["language"],
-                        deploy: repo["homepage"] || (await getGithubPagesUrl(repo["name"])),
-                    };
-                })
-            );
-        })
-        .catch((error) => {
-            console.log("ERROR while fetching user repos: ", error);
-        });
+export async function getRepos() {
+    try {
+        const response = await fetch(new URL("https://api.github.com/users/guidodinello/repos"))
+        const data = await response.json();
+
+        const repos = await Promise.all(
+            data.map(async (repo) => {
+                const deploy = repo["homepage"] || (await getGithubPagesUrl(repo["name"]));
+                return {
+                    title: repo["name"],
+                    description: repo["description"],
+                    url: repo["html_url"],
+                    tags: repo["topics"],
+                    language: repo["language"],
+                    deploy
+                };
+            })
+        );
+        return repos;
+    } catch (error) {
+        console.log("ERROR while fetching user repos: ", error);
+        return [];
+    }
 }
 
 async function getGithubPagesUrl(repo_name) {
     /* workaround, github api doesnt return gh pages url with the repo info */
     try {
-        const response = await fetch(
-            `https://guidodinello.github.io/${repo_name}`,
-            { method: "HEAD" }
-        );
+        const repoURL = new URL(`https://guidodinello.github.io/${repo_name}`)
+        const response = await fetch(repoURL, { method: "HEAD" });
         if (response.status === 404) return null;
-        else return `https://guidodinello.github.io/${repo_name}`;
+        else return repoURL;
     } catch (error) {
         console.log("ERROR with REPO", repo_name);
     }
