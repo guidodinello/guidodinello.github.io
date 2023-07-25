8adb694c9085448f69736fd28bc26472e24f01e4
Guido Dinello <guido.dinello@gmail.com>
2023-07-25 00:04:40 -0300

    eslint rules added and code formatted to comply them

--- js/utils/github.js
+++ js/utils/github.js
@@ -1,11 +1,14 @@
 export async function getRepos() {
     try {
-        const response = await fetch(new URL("https://api.github.com/users/guidodinello/repos"))
+        const response = await fetch(
+            new URL("https://api.github.com/users/guidodinello/repos")
+        );
         const data = await response.json();
 
         const repos = await Promise.all(
-            data.map(async (repo) => {
-                const deploy = repo["homepage"] || (await getGithubPagesUrl(repo["name"]));
+            data.map(async(repo) => {
+                const deploy = 
+                    repo["homepage"] || (await getGithubPagesUrl(repo["name"]));
                 return {
                     title: repo["name"],
                     description: repo["description"],
@@ -27,10 +30,10 @@ export async function getRepos() {
 async function getGithubPagesUrl(repo_name) {
     /* workaround, github api doesnt return gh pages url with the repo info */
     try {
-        const repoURL = new URL(`https://guidodinello.github.io/${repo_name}`)
+        const repoURL = new URL(`https://guidodinello.github.io/${repo_name}`);
         const response = await fetch(repoURL, { method: "HEAD" });
-        if (response.status === 404) return null;
-        else return repoURL;
+        if (response.status === 404) {return null;}
+        else {return repoURL;}
     } catch (error) {
         console.log("ERROR with REPO", repo_name);
     }
